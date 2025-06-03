6e682e2b6a4ff6c824fb21a78d95a82d72de20fa
Bazhenov Kirill <kbazenov000@gmail.com>
2025-06-03 21:13:34 +0400

    Auto user's id setter

--- internal/controllers/registr.go
+++ internal/controllers/registr.go
@@ -13,11 +13,17 @@ import (
 func Registr(c *gin.Context) {
 	user := models.User{}
 
+	var usersCount int64
+	database.DB.Raw("SELECT COUNT(*) FROM users").Scan(&usersCount)
+
+	user.User_id = int(usersCount) + 1
+
 	body, err := io.ReadAll(c.Request.Body)
 	if err != nil {
 		fmt.Errorf("Ошибка при чтении запроса: %w", err)
 	}
 
 	json.Unmarshal(body, &user)
+
 	database.DB.Create(&user)
 }
